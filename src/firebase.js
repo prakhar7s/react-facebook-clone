3e994654d0a810a9a54bc6ec947f21b38bfb3c96
prakhar7s <prakharshrivastava971@gmail.com>
2021-07-19 01:51:28 +0530

    update: packages updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDKxkLEgv79tMINp0mE9Xfv77_Uev9XSNE",
-  authDomain: "clwn-clothing-6890b.firebaseapp.com",
-  projectId: "clwn-clothing-6890b",
-  storageBucket: "clwn-clothing-6890b.appspot.com",
-  messagingSenderId: "345214303241",
-  appId: "1:345214303241:web:dab183d5acbe4b492f8cd8"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
